f305b9cb691575c46e83f5e2fad01008b9c39548
DUDEYRUDEY <101237243+DUDEYRUDEY@users.noreply.github.com>
2024-12-07 17:42:33 +1100

    Updated button and containers

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -7,16 +7,14 @@ function Home() {
     return (
         <div className="home-wrapper">
             <div className="hero">
-                <div className="home-container hero--modifications">
+                <div className="container hero--margin">
                     <h1 className="hero__heading">Capstone <span className= "hero__year">2025</span></h1>
-                    <div className='hero__btn-text-wrapper'>
-                        <p className="hero__text">We're super excited your here</p>
-                        <Button buttonType="primary" size="large"/> 
-                    </div> 
+                    <p className="hero__text">We're super excited your here</p>
+                    <Button buttonType="primary" size="medium"/>
                 </div>
             </div>
             <div className="project">
-                <div className="home-container project--navigation">
+                <div className="container project--navigation">
                     <h1>Looking for a project?</h1>
                     <p>Projects are located on the first and first floor of the ACT building</p>
                     <p>Click on the links below to find out more..</p>
