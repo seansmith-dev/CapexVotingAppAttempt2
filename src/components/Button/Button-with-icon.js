ca4e7194f715b351913416c5b9dd56d98a59185d
DUDEYRUDEY <101237243+DUDEYRUDEY@users.noreply.github.com>
2025-02-03 13:30:56 +1100

    change

--- src/components/Button/Button-with-icon.js
+++ src/components/Button/Button-with-icon.js
@@ -5,7 +5,11 @@ import { useNavigate } from 'react-router-dom';
 const ButtonWithIcon = (props) => {
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleClick = (event) => {
+    if(props.onClick){
+      props.onClick(event);
+    }
+
     if (props.buttonNavigateTo) {
       navigate(props.buttonNavigateTo); 
     }
