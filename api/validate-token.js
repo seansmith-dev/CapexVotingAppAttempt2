24c3bd0a7b7d6d64e5ced3f3007577191ea99480
DUDEYRUDEY <101237243+DUDEYRUDEY@users.noreply.github.com>
2025-02-03 14:53:54 +1100

    errors

--- api/validate-token.js
+++ api/validate-token.js
@@ -2,10 +2,11 @@ let validTokens = new Set(); // Same in-memory store as generate-qr.js
 
 export default function handler(req, res) {
     if (req.method !== "GET") {
+        console.log("invalid");
         return res.status(405).json({ error: "Method not allowed" });
     }
-
-    const { token } = req.query.token;
+    console.log("api is atleast called")
+    const { token } = req.query;
     console.log("Received token from frontend:", token);
     
     if (!token || !validTokens.has(token)) {
