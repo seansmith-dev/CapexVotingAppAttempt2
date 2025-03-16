6a77a9a7131c2e10e2170795d67553024f78dfa7
DUDEYRUDEY <101237243+DUDEYRUDEY@users.noreply.github.com>
2025-03-16 12:27:48 +1100

    sdfdfss

--- src/pages/ProjectsList/ProjectsList.js
+++ src/pages/ProjectsList/ProjectsList.js
@@ -6,17 +6,28 @@ import { Link } from 'react-router-dom';
 function ProjectsList() {
     const [projects, setProjects] = useState([]);
 
+    
     useEffect(() => {
-        // Adjust fields as needed
-        const fields = "project_id, project_number,title,no_votes, short_description, faculty"; // specify the fields you want
-        fetch(`/api/projects?fields=${fields}`)
-            .then(response => {
-                console.log(response.status); 
-                return response.json();
+        fetch(`/api/getProjectsList.js`)
+            .then((response) => response.json())
+            .then((data) => {
+                setProjects(data);
+                console.log(data)
             })
-            .then(data => setProjects(data))
-            .catch(error => console.error("Error fetching projects:", error));
-    }, []);
+            .catch((error) => {
+                console.error("Error fetching projects:", error);
+                setError("Error fetching projects.");
+                navigate("/error");
+            });
+    }, [navigate]);
+
+    if (error) {
+        return <div>{error}</div>;
+    }
+
+    if (projects === null) {
+        return <Loading />; // Show loading only while fetching
+    }
 
     return (
         <div className="project-menu">
@@ -31,7 +42,7 @@ function ProjectsList() {
                             <div className="project-list__card">
                                 <h3 className="pl__card-title">Project {project.project_number}</h3>
                                 <p className="pl__short-description">{project.short_description}</p>
-                                <p className="pl__faculty small--text">{project.faculty}</p>
+                                <p className="pl__faculty small--text">{project.faculty_name}</p>
                                 <ButtonWithIcon
                                     buttonType="primary"
                                     size="medium-small"
