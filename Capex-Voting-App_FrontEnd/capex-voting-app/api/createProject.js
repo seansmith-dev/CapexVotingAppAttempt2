2cc16f6eb3299980af17571d85e8754e4c2c641a
DUDEYRUDEY <101237243+DUDEYRUDEY@users.noreply.github.com>
2025-04-27 18:46:44 +1000

    change

--- Capex-Voting-App_FrontEnd/capex-voting-app/api/createProject.js
+++ Capex-Voting-App_FrontEnd/capex-voting-app/api/createProject.js
@@ -1,4 +1,5 @@
-import pkg from 'pg';  // Default import of the entire 'pg' module
+
+import { Pool } from 'pg'; 
 const { Pool } = pkg;  // Destructure 'Pool' from the 'pg' module
 
 const pool = new Pool({
